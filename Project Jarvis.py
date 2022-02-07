3ed721cf28f15e262bab93c6f90803ed81b8f4f5
Tarush Mihir <tarush_b200296ee@nitc.ac.in>
2022-02-07 16:34:24 +0530

    Update Project Jarvis.py

--- Project Jarvis.py
+++ Project Jarvis.py
@@ -8,27 +8,30 @@ import smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
+# print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
     if hour>=0 and hour<12:
-        speak("Good Morning! Mihir")
+        speak("Good Morning!")
 
     elif hour>=12 and hour<18:
-        speak("Good Afternoon! Mihir")   
+        speak("Good Afternoon!")   
 
     else:
-        speak("Good Evening! Mihir")  
+        speak("Good Evening!")  
 
-    speak("I am NEO from matrix of Universe. How can I help you sir!")       
+    speak("I am Neo from Matrix. You can ask your wish and I will grant them.")       
 
 def takeCommand():
-      r = sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshold = 1
@@ -40,7 +43,6 @@ def takeCommand():
         print(f"User said: {query}\n")
 
     except Exception as e:
-        # print(e)    
         print("Say that again please...")  
         return "None"
     return query
@@ -49,17 +51,15 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('mihirtarush@gmail.com', 'password') #I have hidden my password for privacy concern. 
-    server.sendmail('sid.mihir123@gmail.com', to, content)
+    server.login('mihirtarush@gmail.com', '12345678')
+    server.sendmail('sid.mihir@gmail.com', to, content)
     server.close()
 
 if __name__ == "__main__":
     wishMe()
     while True:
-    # if 1:
         query = takeCommand().lower()
 
-        # Logic for executing tasks based on query
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
@@ -79,7 +79,7 @@ if __name__ == "__main__":
 
 
         elif 'play music' in query:
-            music_dir = 'Music'
+            music_dir = 'D:\song'
             songs = os.listdir(music_dir)
             print(songs)    
             os.startfile(os.path.join(music_dir, songs[0]))
@@ -88,13 +88,17 @@ if __name__ == "__main__":
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
             speak(f"Sir, the time is {strTime}")
 
+        elif 'open code' in query:
+            codePath = "C:\Users\mihir\Desktop\Python"
+            os.startfile(codePath)
+
         elif 'email to Mihir' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
                 to = "sid.mihir123@gmail.com"    
                 sendEmail(to, content)
-                speak("I have sent the mail Sir")
+                speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Oops, I encountered an error in matrix and That assigned task could not be completed.")  
+                speak("There is some error due to which I am not able to send the mail.")    
